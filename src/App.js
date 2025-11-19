d436fc87507b6de647e72d32686e0494d4a8f2b3
Chander Shekhar Pal <50290541+cspal12@users.noreply.github.com>
2025-11-19 15:43:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Contact from "./assets/Contact";
 import About from "./Component/About";
-import Experience from "./Component/Experience";
+
 import Home from "./Component/Home";
 import Navbar from "./Component/Navbar";
-import Portfolio from "./Component/Portfolio";
+
 import SocialLinks from "./Component/SocialLinks";
 
 function App() {

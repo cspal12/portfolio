23fc4cc8063a08d32e738b37f7a450dfe0f9fc15
Chander Shekhar Pal <50290541+cspal12@users.noreply.github.com>
2025-11-19 15:35:13 +0530

    Update Experience.jsx

--- src/Component/Experience.jsx
+++ src/Component/Experience.jsx
@@ -25,32 +25,5 @@ const Experience = () => {
   ];
 
   return (
-    <div
-      name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
-    >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div className="pt-8">
-          <p className="text-4xl font-bold border-b-4 border-gray-500 inline">
-            Experience
-          </p>
-          <p className="py-6"> These are the technologies I've workded with</p>
-        </div>
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {skills.map(({ id, src, title, style }) => (
-            <div
-              key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg  ${style}`}
-            >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default Experience;

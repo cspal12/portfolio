67272e90c76ab3226ac78138007c82b846a45110
Chander Shekhar Pal <50290541+cspal12@users.noreply.github.com>
2025-12-11 06:37:11 +0530

    Update Home.jsx

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       >
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm Manual QA Tester ensuring flawless user experiences 
+            I'm Jr.QA Engineer ensuring flawless user experiences 
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
           Manual QA Tester seeking opportunities to deliver reliable, bug‑free products
